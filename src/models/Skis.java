bc02f0706fc31e012005db66c9e65f3c5faf777e
200466543 <77888976+200466543@users.noreply.github.com>
2021-04-22 12:24:11 -0400

    Initial commit

---
+++ src/models/Skis.java
@@ -0,0 +1,34 @@
+/*Nathan Goddard 200466543
+ */
+package models;
+
+import Utilities.DBUtility;
+
+public class Skis {
+    private String brand, model;
+    private int length;
+
+    public Skis(String brand, String model, int length,double purchasePrice, double sellingPrice, int quantityInStock) {
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+    }
+}
